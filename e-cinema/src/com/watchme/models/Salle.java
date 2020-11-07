bf27f0a48e382883e5aa5b974e33b4a6df12ff28
AZComputer <AZComputer@192.168.0.18>
2020-11-07 18:27:54 +0100

    Salle

--- e-cinema/src/com/watchme/models/Salle.java
+++ e-cinema/src/com/watchme/models/Salle.java
@@ -1,6 +1,77 @@
 package com.watchme.models;
 
-public class Salle {
+import java.io.Serializable;
+import java.util.List;
 
-	
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.watchme.models.Programme;
+
+@Entity
+public class Salle implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	private Integer numero;
+	private Integer nombrePlace;
+	//bi-directional one-to-many association to Programme
+	@OneToMany(mappedBy="salle")
+	private List<Programme> programmes;
+
+	// Constructors
+	public Salle(Integer numero, Integer nombrePlace) {
+		super();
+		this.numero = numero;
+		this.nombrePlace = nombrePlace;
+	}
+
+	public Salle() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public Integer getNombrePlace() {
+		return nombrePlace;
+	}
+
+	public void setNombrePlace(Integer nombrePlace) {
+		this.nombrePlace = nombrePlace;
+	}
+
+	public List<Programme> getProgrammes() {
+		return programmes;
+	}
+
+	public void setProgrammes(List<Programme> programmes) {
+		this.programmes = programmes;
+	}
+
 }
